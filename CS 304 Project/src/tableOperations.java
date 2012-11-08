bf1f99100338ce864ccab7fdd3f4f2b2e72560f4
yolandaw <Yolanda@206.87.134.248>
2012-11-08 11:31:43 -0800

    created a class for Table Ops (insert, delete, display to be added)

---
+++ CS 304 Project/src/tableOperations.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author Yolanda
+ *
+ */
+public class tableOperations {
+
+	/**
+	 * 
+	 */
+	public tableOperations() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
