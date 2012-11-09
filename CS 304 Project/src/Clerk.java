5dce38425b85b2a97fd34190ac8106fe72007109
yolandaw <Yolanda@206.87.135.197>
2012-11-08 22:19:14 -0800

    added methods to clerk class

--- CS 304 Project/src/Clerk.java
+++ CS 304 Project/src/Clerk.java
@@ -5,6 +5,20 @@ public class Clerk{
 	public Clerk() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public void addBorrower(){
+			
+	}
+	
+	public void checkOut(){
+		
+	}
+	
+	public void bookReturn(){
+		
+	}
 
-
+	public void checkOverdue(){
+		
+	}
 }
