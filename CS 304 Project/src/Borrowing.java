550b0d91134f62cfaf7fb227a58b3b10c8c5b40b
Cathy <licathy06@gmail.com>
2012-11-15 20:48:40 -0800

    updated 2 classes

--- CS 304 Project/src/Borrowing.java
+++ CS 304 Project/src/Borrowing.java
@@ -1,24 +1,76 @@
+import java.io.*;
+import java.sql.*;
 
 public class Borrowing {
 	
 	public Borrowing() {
 		
 	}
+	private Connection con;
+	private BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 	
 	// Insert a tuple into the table Borrowing
-	public void insertBorrowing() {
-		
+	public void insertBorrowing(int borid, int bid, int callNo, int copyNo, Date outDate, Date inDate) {
+		try {
+			PreparedStatement ps = con.prepareStatement("INSERT INTO borrowing VALUES(?,?,?,?,?,?)");
+			ps.setInt(1, borid);
+			ps.setInt(2, bid);
+			ps.setInt(3, callNo);
+			ps.setInt(4, copyNo);
+			ps.setDate(5, outDate);
+			ps.setDate(6, inDate);
+			ps.executeUpdate();
+			con.commit();
+			ps.close();
+		}
+		catch (SQLException ex)
+		{
+			System.out.println("Message: " + ex.getMessage());
+		    try 
+		    {
+			// undo the insert
+			con.rollback();	
+		    }
+		    catch (SQLException ex2)
+		    {
+			System.out.println("Message: " + ex2.getMessage());
+			System.exit(-1);
+		    }
+
+		}
 	}
 	
 	// Delete a tuple from the table Borrowing
-	public void deleteBorrowing() {
+	public void deleteBorrowing(int borid) {
+		PreparedStatement ps;
 		
+		try {
+			ps = con.prepareStatement("DELETE FROM borrowing WHERE borrowing_borid = ?");
+			ps.setInt(1, borid);
+			ps.executeUpdate();
+			
+			con.commit();
+			ps.close();
+		}
+		catch (SQLException ex)
+		{
+		    System.out.println("Message: " + ex.getMessage());
+
+		    try 
+		    {
+		    	con.rollback();	
+		    }
+		    catch (SQLException ex2)
+		    {
+		    	System.out.println("Message: " + ex2.getMessage());
+		    	System.exit(-1);
+		    }
+		}
 	}
 	
 	// Display all the rows of the table Borrowing
-	public int displayBorrowing() {
-		return 0;
-		
+	public void displayBorrowing() {
+		//TODO
 	}
 
 }
--- CS 304 Project/src/bookCopy.java
+++ CS 304 Project/src/bookCopy.java
@@ -1,24 +1,105 @@
+import java.io.*;
+import java.sql.*;
 
 public class bookCopy {
 	
 	public bookCopy() {
 		
 	}
+	private Connection con;
+	private BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 	
 	// Insert a tuple into the table BookCopy
-	public void insertBookCopy() {
-		
+	public void insertBookCopy(int callNo, int copyNo, int status) {
+		PreparedStatement ps;
+		try {
+			ps = con.prepareStatement("INSERT INTO bookCopy VALUES (?,?,?)");
+			ps.setInt(1, callNo);
+			ps.setInt(2, copyNo);
+			ps.setInt(3, status);
+			
+			ps.executeUpdate();
+			con.commit();
+			ps.close();
+		}
+		catch (SQLException ex)
+		{
+		    System.out.println("Message: " + ex.getMessage());
+		    try 
+		    {
+		    	// undo the insert
+		    	con.rollback();	
+		    }
+		    catch (SQLException ex2)
+		    {
+		    	System.out.println("Message: " + ex2.getMessage());
+		    	System.exit(-1);
+		    }
+		}
+
 	}
 	
 	// Delete a tuple from the table BookCopy
-	public void deleteBookCopy() {
-		
+	public void deleteBookCopy(int callNo, int copyNo) {
+		PreparedStatement ps;
+		try {
+			ps = con.prepareStatement("DELETE FROM bookCopy WHERE book_callNo = ? AND bookCopy_copyNo = ?");
+			ps.setInt(1, callNo);
+			ps.setInt(2, copyNo);
+			ps.executeUpdate();
+			
+			con.commit();
+			ps.close();
+		}
+
+		catch (SQLException ex)
+		{
+		    System.out.println("Message: " + ex.getMessage());
+
+		    try 
+		    {
+		    	con.rollback();	
+		    }
+		    catch (SQLException ex2)
+		    {
+		    	System.out.println("Message: " + ex2.getMessage());
+		    	System.exit(-1);
+		    }
+		}
+
 	}
 		
 	// Display all the rows of the table BookCopy
-	public int displayBookCopy() {
-		return 0;
+	public void displayBookCopy() {
+		int callNo;
+		int copyNo;
+		int status;
+		Statement stmt;
+		ResultSet rs;
 		
+//		try {
+//			stmt = con.createStatement();
+//			rs = stmt.executeQuery("SELECT * FROM bookCopy");
+//			ResultSetMetaData rsmd = rs.getMetaData();
+//			int numCols = rsmd.getColumnCount();
+//			System.out.println("");
+//			for (int i = 0; i < numCols; i++)
+//			{
+//				// get column name and print it
+//
+//				System.out.printf("%-15s", rsmd.getColumnName(i+1));  
+//			}
+//			System.out.println("");
+//			
+//			while (rs.next()) {
+//				callNo = rs.getInt("book_callNo");
+//				System.out.printf("%-10.10s", callNo);
+//				
+//				copyNo = rs.getInt("bookCopy_copyNo");
+//				
+//			}
+//
+//		}
 	}
 
 }
