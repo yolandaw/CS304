0b900a64889eb4c10325e0c9b4ac2403f6c3ef81
yolandaw <Yolanda@206.87.133.106>
2012-11-16 23:56:27 -0800

    commiting unfinished clerk class

--- CS 304 Project/src/Clerk.java
+++ CS 304 Project/src/Clerk.java
@@ -3,6 +3,8 @@ import java.sql.*;
 //only added some trivial stuff, i'll continue after friday if nobody has done anything - Yolanda
 
 public class Clerk{
+	
+	java.sql.Connection con = Connection.getInstance().getConnection();
 
 	public Clerk() {
 		// TODO Auto-generated constructor stub
@@ -14,7 +16,8 @@ public class Clerk{
 	}
 	
 	public void checkOut(int bid, int callNo ){
-	
+		
+		PreparedStatement ps;	
 	}
 	
 	public void bookReturn(){
@@ -22,6 +25,22 @@ public class Clerk{
 	}
 
 	public void checkOverdue(){
+		
+	}
+	
+	public boolean checkStatus(int bid){
+		Statement stmt;
+		ResultSet rs;
+		try{
+		stmt = con.createStatement();
+		rs = stmt.executeQuery("SELECT bt_type FROM borrower WHERE borr_bid = "+ bid );
+		
+		}
+		catch (SQLException ex)
+		{
+			System.out.println("Message:"+ex.getMessage());
+		}
+		return false;
 		
 	}
 }
