e16969834b7585495a37839dff2b71c8c4b2679c
Shirley Yin <sshirley.yin@gmail.com>
2012-11-16 18:15:58 -0800

    removed old connection part

--- CS 304 Project/src/hasSubject.java
+++ CS 304 Project/src/hasSubject.java
@@ -7,30 +7,6 @@ public class hasSubject {
 	
 	public hasSubject() {
 
-	      try 
-	      {
-	    	  // Load the Oracle JDBC driver
-	    	  DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-	      }
-	      catch (SQLException ex)
-	      {
-	    	  System.out.println("Message: " + ex.getMessage());
-	    	  System.exit(-1);
-	      }
-		
-	      String connectURL = "jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1522:ug"; 
-
-	      try 
-	      {
-	    	  	con = DriverManager.getConnection(connectURL,"ora_v2e7","a75190090");
-
-	    	  	System.out.println("\nConnected to Oracle!");
-	      }
-	      catch (SQLException ex)
-	      {
-	    	  	System.out.println("Message: " + ex.getMessage());
-	      }
-
 	}
 	
 	// Insert a tuple into the table HasSubject
