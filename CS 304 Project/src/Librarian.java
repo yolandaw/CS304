65d56090728b55da0125bdd1d8fe938a482e3cf2
Shirley Yin <sshirley.yin@gmail.com>
2012-11-23 20:10:03 -0800

    help me debug generateBookReport()

--- CS 304 Project/src/Librarian.java
+++ CS 304 Project/src/Librarian.java
@@ -17,39 +17,36 @@ public class Librarian {
 		// TODO Auto-generated constructor stub
 	}
 
-	//choice only takes in int 1 or 2 
-	public void addBook (int callNo, String title, String mainAuthor, String publisher, int year, int choice) {
+	public boolean checkISBN(int isbn) {
 
+		Book book = new Book();
+		int callNo = book.checkBook(isbn);
 
-		try {
-
-			System.out.print("\nISBN: ");
-			int isbn = Integer.parseInt(in.readLine());
-
-			Book book = new Book();
-			int existingCallNo = book.checkBook(isbn);
-			if (book.checkBook(isbn) == 0) {
-				if (book.numCopies(callNo) == 0) {	
-					book.insertBook(callNo, isbn, title, mainAuthor, publisher, year);			
-				} else {
-					System.out.print("\nCall Number exists, perhaps there was an error in entering the ISBN or Call Number.");	
-					return;
-				}
-				
-				addCopy(callNo, choice);
-			} else {
-				System.out.print("\nBook exists, adding new copy:");
-				addCopy(existingCallNo, choice);
-			}
-		}
-
-		catch (IOException e)
-		{
-			System.out.println("IOException!");
+		if (callNo == 0) {
+			return false;
+		} else {
+			return true;
 		}
 	}
 
-	//choice only takes in int 1 or 2 
+	public void addBook (int callNo, int isbn, String title, String mainAuthor, String publisher, int year) {
+
+		Book book = new Book();
+		//			int existingCallNo = book.checkBook(isbn);
+		//			if (book.checkBook(isbn) == 0) {
+		//				if (book.numCopies(callNo) == 0) {	
+		book.insertBook(callNo, isbn, title, mainAuthor, publisher, year);			
+		//				} else {
+		//					return;
+		//				}
+		//				
+		//				addCopy(callNo, choice);
+		//			} else {
+		//				addCopy(existingCallNo, choice);
+		//			}
+
+	}
+
 	public void addCopy (int callNo, int choice) {
 
 		try {
@@ -57,10 +54,11 @@ public class Librarian {
 			String status = null;
 			switch(choice)
 			{
-			   case 1: status = "in"; break;
-			   case 2: status = "out"; break;
+			case 1: status = "in"; break;
+			case 2: status = "out"; break;
+			case 3: status = "on-hold"; break;
 			}
-			
+
 			bookCopy copies = new bookCopy();
 
 
@@ -72,27 +70,46 @@ public class Librarian {
 
 
 
-	public ResultSet generateBookReport() {
+	public void generateBookReport() {
 
-		int bid;
 		int borid;
-		String callNo;
-		String copyNo;
-		Date outDate;
-		Date inDate;
+		String overdue;
 		Statement stmt;
 		ResultSet rs;
 		Borrowing borrowing = new Borrowing();
-		
+
 		try{
 			stmt = con.createStatement();
-			rs = stmt.executeQuery("SELECT borrowing_borid, borr_bid, book_callNo, bookCopy_copyNo, borrowing_outDate, borrowing_inDate FROM borrowing WHERE borrowing_inDate IS NULL");	
+			rs = stmt.executeQuery("SELECT borrowing_borid, borr_bid, book_callNo, bookCopy_copyNo, borrowing_outDate, borrowing_inDate FROM borrowing WHERE borrowing_inDate IS NULL");
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int numCols = rsmd.getColumnCount();
 
+			List<List<String>> colArray = new ArrayList<List<String>>();
+
+			while(rs.next()) {
+
+				borid = rs.getInt("borrowing_borid");
 				if (borrowing.isOverdue(borid)) {
-		
+					overdue = "Overdue";	
 				} else {
-					System.out.printf("%-20.20s", "");
+					overdue = "";
 				}
+				
+				List<String> rowArray = new ArrayList<String>();
+				for (int i=1; i <= numCols; i++) {
+					rowArray.add(i, rs.getString(i));
+				}
+				rowArray.add(overdue);
+				colArray.add(rowArray);
+			}
+			
+			int numRows = colArray.size();
+			System.out.println(numRows);
+			for (int i=1; i <= numRows; i++) {
+				for (int j=1; j <= 7; j++) {
+					System.out.println(colArray.get(i).get(j));
+				}
+			}
 			
 			stmt.close();
 		}
@@ -100,7 +117,7 @@ public class Librarian {
 		catch(SQLException e){
 			System.out.print("Message: " + e.getMessage());
 		}
-		
+
 	}
 
 	public void generatePopularBooksReport(int year, int n) {
@@ -109,24 +126,24 @@ public class Librarian {
 		int count;
 		Statement stmt;
 		ResultSet rs;
-		
+
 		try{
 			stmt = con.createStatement();
 			rs = stmt.executeQuery("SELECT book_callNo, COUNT(borrowing_borid) FROM borrowing WHERE EXTRACT(YEAR from borrowing_outDate) ='" + year + "' GROUP BY book_callNo ORDER BY COUNT(borrowing_borid) DESC");
 			ResultSetMetaData rsmd = rs.getMetaData();
 			int numCols = rsmd.getColumnCount();
-			
+
 			System.out.println("\n\n");
-			
+
 			for (int i = 0; i < numCols; i++)
 			{
 				// get column name and print it
 				System.out.printf("%-15s", rsmd.getColumnName(i+1));    
 			}
-			
+
 			int j = 1;
 			while(rs.next() && j <= n) {
-				
+
 				callNo = rs.getInt("book_callNo");
 				System.out.printf("\n%-10.10s", callNo);
 
@@ -141,7 +158,7 @@ public class Librarian {
 			System.out.print("Message: " + e.getMessage());
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		//Schedule a job for the event-dispatching thread:
 		//creating and showing this application's GUI.
@@ -150,11 +167,13 @@ public class Librarian {
 				Librarian librarian = new Librarian();
 				//     testTable.insertBook(3, 2, "a", "b", "c", 0);
 
-				librarian.generateBookReport();    
-				
-				librarian.generatePopularBooksReport(2012, 1);
+				//			librarian.generateBookReport();    
 
-				librarian.addBook(, title, mainAuthor, publisher, year, choice)
+				//		librarian.generatePopularBooksReport(2012, 1);
+
+			//	librarian.addBook(48, 0, "title", "mainAuthor", "publisher", 1939);
+			
+			librarian.generateBookReport();
 			}
 		});
 	}
