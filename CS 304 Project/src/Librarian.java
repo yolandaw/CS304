8242bd80fc07caffbe536ba922415da27c0fc465
Shirley Yin <sshirley.yin@gmail.com>
2012-11-24 17:30:11 -0800

    added column headings to array

--- CS 304 Project/src/Librarian.java
+++ CS 304 Project/src/Librarian.java
@@ -75,12 +75,17 @@ public class Librarian {
 
 		try{
 			stmt = con.createStatement();
-			rs = stmt.executeQuery("SELECT borrowing_borid, borr_bid, book_callNo, bookCopy_copyNo, borrowing_outDate, borrowing_inDate FROM borrowing WHERE borrowing_inDate IS NULL");
-			ResultSetMetaData rsmd = rs.getMetaData();
-			int numCols = rsmd.getColumnCount();
-
+			rs = stmt.executeQuery("SELECT borrowing_borid, book_callNo, bookCopy_copyNo, borrowing_outDate, borrowing_inDate FROM borrowing WHERE borrowing_inDate IS NULL");
+			
 			List<List<String>> colArray = new ArrayList<List<String>>(1);
 
+			List<String> columnHeadingsArray = new ArrayList<String>(1);
+			columnHeadingsArray.add("Call Number");
+			columnHeadingsArray.add("Copy Number");
+			columnHeadingsArray.add("Out Date");
+			columnHeadingsArray.add("Overdue");
+			colArray.add(columnHeadingsArray);
+			
 			while(rs.next()) {
 
 				borid = rs.getInt("borrowing_borid");
@@ -91,15 +96,25 @@ public class Librarian {
 				}
 				
 				List<String> rowArray = new ArrayList<String>(1);
-				for (int i=0; i < numCols; i++) {
-					rowArray.add(i, rs.getString(i+1));
-				}
+
+				rowArray.add(rs.getString("book_callNo"));
+				rowArray.add(rs.getString("bookCopy_copyNo"));
+				rowArray.add(rs.getString("borrowing_outDate"));
 				rowArray.add(overdue);
 				colArray.add(rowArray);
-				return colArray;
+
 			}
 						
 			stmt.close();
+			
+//			int numRows = colArray.size();
+//			for (int i=0; i < numRows; i++) {
+//				for (int k=0; k < 4; k++) {
+//					System.out.println(colArray.get(i).get(k));
+//				}
+//			}
+			
+			return colArray;
 		}
 
 		catch(SQLException e){
@@ -116,26 +131,33 @@ public class Librarian {
 
 		try{
 			stmt = con.createStatement();
-			rs = stmt.executeQuery("SELECT book_callNo, COUNT(*) FROM borrowing WHERE EXTRACT(YEAR from borrowing_outDate) ='" + year + "' GROUP BY book_callNo ORDER BY COUNT(borrowing_borid) DESC");
-			ResultSetMetaData rsmd = rs.getMetaData();
-			int numCols = rsmd.getColumnCount();
+			rs = stmt.executeQuery("SELECT * FROM ((SELECT book_callNo, COUNT(borrowing_borid) FROM borrowing WHERE EXTRACT(YEAR from borrowing_outDate) ='" + year + "' GROUP BY borrowing.book_callNo ORDER BY COUNT(borrowing_borid) DESC) borr JOIN (SELECT book_callNo, book_title from book) booktitle ON borr.book_callNo = booktitle.book_callNo)");
 
 			List<List<String>> colArray = new ArrayList<List<String>>(1);
+
+
+			List<String> columnHeadingsArray = new ArrayList<String>(1);
+			columnHeadingsArray.add("Call Number");
+			columnHeadingsArray.add("Book Title");
+			columnHeadingsArray.add("Number of Times Borrowed");
+			colArray.add(columnHeadingsArray);
+			
 			int j = 1;
 			
 			while(rs.next() && j <= n) {
 				
 				List<String> rowArray = new ArrayList<String>(1);
-				for (int i=0; i < numCols; i++) {
-					rowArray.add(i, rs.getString(i+1));
-				}
+
+				rowArray.add(rs.getString("book_callNo"));
+				rowArray.add(rs.getString("book_title"));
+				rowArray.add(rs.getString("COUNT(borrowing_borid)"));
 				colArray.add(rowArray);
 				
 			}
 			
 //			int numRows = colArray.size();
 //			for (int i=0; i < numRows; i++) {
-//				for (int k=0; k < numCols; k++) {
+//				for (int k=0; k < 3; k++) {
 //					System.out.println(colArray.get(i).get(k));
 //				}
 //			}
@@ -164,7 +186,8 @@ public class Librarian {
 
 			//	librarian.addBook(48, 0, "title", "mainAuthor", "publisher", 1939);
 			
-			librarian.generatePopularBooksReport(2012, 10);
+//			librarian.generateBookReport();
+//			librarian.generatePopularBooksReport(2012, 10);
 			}
 		});
 	}
